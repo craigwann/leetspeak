d88e3aa407e43d2896a351ffc4d0a86c5086c336
Craig Wann and Eric Shull <eric@>
2018-06-25 14:22:22 -0700

    first positive fail and pass for returning a string

---
+++ lib/leetspeak.rb
@@ -0,0 +1,15 @@
+class String
+  def leetspeak
+
+    # string_array = []
+    # string_array.push(sentence)
+"happy"
+  end
+end
+#
+# def leetspeak(word)
+#   reversed_string = self.reverse()
+#   # string_array = []
+#   # string_array.push(sentence)
+# reversed_string
+# end
---
+++ spec/leetspeak_spec.rb
@@ -0,0 +1,20 @@
+require('rspec')
+require('leetspeak')
+
+# describe('String#leetspeak') do
+#   it('returns a string as is when no Leetspeak rules apply') do
+#     expect(leetspeak("happy")).to(eq("happy"))
+#   end
+# end
+
+describe('String#leetspeak') do
+  it('returns a string as is when no Leetspeak rules apply') do
+    expect("happy".leetspeak()).to(eq("happy"))
+  end
+end
+
+# describe('String#leetspeak') do
+#   it('returns a string as is when no Leetspeak rules apply') do
+#     expect(leetspeak("happy")).to(eq("happy"))
+#   end
+# end
