8184c5d32a2f9e8ee2fe6a1ec4bb6cd0ecb3c19b
Craig Wann and Eric Shull <eric@>
2018-06-25 16:29:24 -0700

    work on first letter of word s thing

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,13 +1,24 @@
 class String
   def leetspeak
     leet_word = self.tr('e, o, I, s', '3, 0, 1, z')
+    if leet_word.start_with?('s')
 
-
+      # don't replace with z
+    else
+      leet_word.tr('s', 'z')
+      # replace with z
     # string_array = []
     # string_array.push(sentence)
 
   end
 end
+
+def first_letter (x)
+  new_array = []
+  # if (x.index('s'))
+    x.each do |array_element|
+      new_array.(array_element)
+
 
 
 #
